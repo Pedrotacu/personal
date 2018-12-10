e1156e96ab361ec23221ae6d0acb6ac7de95d43c
Pedro <Pedro@laptop-Pedro.lan>
2018-12-10 19:03:07 +0100

    ?

---
+++ Esercizio01/src/PrimaProva/HelloWorld.java
@@ -0,0 +1,20 @@
+package PrimaProva;
+
+public class HelloWorld {
+	public static void main (String[] args) {
+		//stampa ed esegue calcoli
+		System.out.println(5+5);
+		System.out.println(100*2);
+		System.out.println(10/2);
+		System.out.println(5-1);
+		
+		//stampa riga per dividere
+		System.out.println("-------------------");
+		
+		//stampa a stringa ".."
+		System.out.println("5+5");
+		
+		
+	
+	}
+}
